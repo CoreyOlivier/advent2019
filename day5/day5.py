37e353a9b49511fb14d3f71106acc6c4439091be
Corey Olivier <coreyolivier@gmail.com>
2019-12-07 08:47:53 -0600

    day5 complete

--- day5/day5.py
+++ day5/day5.py
@@ -90,26 +90,148 @@ def op4(prog, position, modes):
         print('error op4 at position: ' + str(position))
 
 
-def op5(prog, position):
-    try:
-        if bool(prog[position + 1]) is True:
-            position = prog[position + 2]
+def op5(prog, position, modes):
+    if modes['mode0'] == 'position':
+        if bool(prog[prog[position +1]]) is True:
+            if modes['mode1'] == 'position':
+                position = prog[prog[position + 2]]
+            else:
+                position = prog[position + 2]
         else:
             position += 3
-
-        return position
-    except Exception:
-        print('error op5 at position: ' + str(position))
-
-
-def op6(prog, position):
-    try:
-        if bool(prog[position + 1]) is False:
-            position = prog[position + 2]
+    else:
+        if bool(prog[position +1]) is True:
+            if modes['mode1'] == 'position':
+                position = prog[prog[position + 2]]
+            else:
+                position = prog[position + 2]
         else:
             position += 3
+    return prog, position
+
+
+def op6(prog, position, modes):
+    if modes['mode0'] == 'position':
+        if bool(prog[prog[position +1]]) is False:
+            if modes['mode1'] == 'position':
+                position = prog[prog[position + 2]]
+            else:
+                position = prog[position + 2]
+        else:
+            position += 3
+    else:
+        if bool(prog[position +1]) is False:
+            if modes['mode1'] == 'position':
+                position = prog[prog[position + 2]]
+            else:
+                position = prog[position + 2]
+        else:
+            position += 3
+    return prog, position
+
+def op7(prog, position, modes):
+    try:
+        if modes['mode0'] and modes['mode1'] == 'position':
+            if modes['mode2'] == 'position':
+                if prog[prog[position + 1]] < prog[prog[position + 2]]:
+                    prog[prog[position + 3]] = 1
+                else:
+                    prog[prog[position + 3]] = 0
+            else:
+                if prog[prog[position + 1]] < prog[prog[position + 2]]:
+                    prog[position + 3] = 1
+                else:
+                    prog[position + 3] = 0
+        elif modes['mode0'] and modes['mode1'] == 'immediate':
+            if modes['mode2'] == 'position':
+                if prog[position + 1] < prog[position + 2]:
+                    prog[prog[position + 3]] = 1
+                else:
+                    prog[prog[position + 3]] = 0
+            else:
+                if prog[prog[position + 1]] < prog[prog[position + 2]]:
+                    prog[position + 3] = 1
+                else:
+                    prog[position + 3] = 0
+        elif modes['mode0'] == 'position 'and modes['mode1'] == 'immediate':
+            if modes['mode2'] == 'position':
+                if prog[prog[position + 1]] < prog[position + 2]:
+                    prog[prog[position + 3]] = 1
+                else:
+                    prog[prog[position + 3]] = 0
+            else:
+                if prog[prog[position + 1]] < prog[prog[position + 2]]:
+                    prog[position + 3] = 1
+                else:
+                    prog[position + 3] = 0
+        elif modes['mode0'] == 'immediate'and modes['mode1'] == 'position':
+            if modes['mode2'] == 'position':
+                if [prog[position + 1]] < prog[prog[position + 2]]:
+                    prog[prog[position + 3]] = 1
+                else:
+                    prog[prog[position + 3]] = 0
+            else:
+                if prog[prog[position + 1]] < prog[prog[position + 2]]:
+                    prog[position + 3] = 1
+                else:
+                    prog[position + 3] = 0
+        
+        return prog, position + 4
+
     except Exception:
-        print('error op5 at position: ' + str(position))
+        print('error op7 at position: ' + str(position))
+
+
+def op8(prog, position, modes):
+    try:
+        if modes['mode0'] and modes['mode1'] == 'position':
+            if modes['mode2'] == 'position':
+                if prog[prog[position + 1]] == prog[prog[position + 2]]:
+                    prog[prog[position + 3]] = 1
+                else:
+                    prog[prog[position + 3]] = 0
+            else:
+                if prog[prog[position + 1]] == prog[prog[position + 2]]:
+                    prog[position + 3] = 1
+                else:
+                    prog[position + 3] = 0
+        elif modes['mode0'] and modes['mode1'] == 'immediate':
+            if modes['mode2'] == 'position':
+                if prog[position + 1] == prog[position + 2]:
+                    prog[prog[position + 3]] = 1
+                else:
+                    prog[prog[position + 3]] = 0
+            else:
+                if prog[prog[position + 1]] == prog[prog[position + 2]]:
+                    prog[position + 3] = 1
+                else:
+                    prog[position + 3] = 0
+        elif modes['mode0'] == 'position 'and modes['mode1'] == 'immediate':
+            if modes['mode2'] == 'position':
+                if prog[prog[position + 1]] == prog[position + 2]:
+                    prog[prog[position + 3]] = 1
+                else:
+                    prog[prog[position + 3]] = 0
+            else:
+                if prog[prog[position + 1]] == prog[prog[position + 2]]:
+                    prog[position + 3] = 1
+                else:
+                    prog[position + 3] = 0
+        elif modes['mode0'] == 'immediate'and modes['mode1'] == 'position':
+            if modes['mode2'] == 'position':
+                if [prog[position + 1]] == prog[prog[position + 2]]:
+                    prog[prog[position + 3]] = 1
+                else:
+                    prog[prog[position + 3]] = 0
+            else:
+                if prog[prog[position + 1]] == prog[prog[position + 2]]:
+                    prog[position + 3] = 1
+                else:
+                    prog[position + 3] = 0
+        return prog, position + 4
+        
+    except Exception:
+        print('error op8 at position: ' + str(position))
 
 
 def parse_instructions(code):
@@ -131,19 +253,52 @@ def part1(prog):
     position = 0
 
     while True:
-        op, modes = parse_instructions(prog[position])
-
+        try:
+            op, modes = parse_instructions(prog[position])
+            
+        except Excpetion:
+            pass
+        finally:
+#            print('position: ' + str(position))
+#            print('modes: ')
+#            print(modes)
+            pass
+            
         if op == '99':
             return prog
         elif op == '01':
+#            print(prog[position], prog[position + 1],
+#                    prog[position + 2], prog[position +3])
             prog, position = op1(prog, position, modes)
         elif op == '02':
+#            print(prog[position], prog[position + 1],
+#                    prog[position + 2], prog[position +3])
             prog, position = op2(prog, position, modes)
         elif op == '03':
+#            print(prog[position], prog[position + 1],
+#                    prog[position + 2], prog[position +3])
             prog, position = op3(prog, position, modes)
         elif op == '04':
+#            print(prog[position], prog[position + 1],
+#                    prog[position + 2], prog[position +3])
             output, position = op4(prog, position, modes)
             print(output)
+        elif op == '05':
+#            print(prog[position], prog[position + 1],
+#                    prog[position + 2], prog[position +3])
+            prog, position = op5(prog, position, modes)
+        elif op == '06':
+#            print(prog[position], prog[position + 1],
+#                    prog[position + 2], prog[position +3])
+            prog, position = op6(prog, position, modes)
+        elif op == '07':
+#            print(prog[position], prog[position + 1],
+#                    prog[position + 2], prog[position +3])
+            prog, position = new_op7(prog, position, modes)
+        elif op == '08':
+#            print(prog[position], prog[position + 1],
+#                    prog[position + 2], prog[position +3])
+            prog, position = new_op8(prog, position, modes)
 
         else:
             print('err')
@@ -151,19 +306,61 @@ def part1(prog):
             return prog, position
 
 
-def foo():
-    v = input('bar?')
-    print(v)
-
-    return v
-
-
-def bar():
-    return foo()
-
 
 prog = parse_file()
 sample0 = [1002, 4, 3, 4, 33]
 sample1 = [1001, 4, 98, 4, 1]
 sample2 = [3, 0, 4, 0, 99]
 sample3 = [1001, 4, 100, 4, -1]
+answers = [ 3545932 ] 
+sample4  = [3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99]
+
+
+def new_op7(prog, position, modes):
+    try:
+        if modes['mode0'] == 'position':
+            value1 = prog[prog[position + 1]]
+            if modes['mode1'] == 'position':
+                value2 = prog[prog[position + 2]]
+            else:
+                value2 = prog[position + 2]
+
+        else:
+            value1 = prog[position + 1]
+            if modes['mode1'] == 'position':
+                value2 = prog[prog[position + 2]]
+            else:
+                value2 = prog[position + 2]
+        if value1 < value2:
+            prog[prog[position + 3]] = 1
+        else:
+            prog[prog[position + 3]] = 0
+        return prog, position + 4
+    except Exception:
+        print('error in new op 7')
+            
+
+def new_op8(prog, position, modes):
+    try:
+        if modes['mode0'] == 'position':
+            value1 = prog[prog[position + 1]]
+            if modes['mode1'] == 'position':
+                value2 = prog[prog[position + 2]]
+            else:
+                value2 = prog[position + 2]
+
+        else:
+            value1 = prog[position + 1]
+            if modes['mode1'] == 'position':
+                value2 = prog[prog[position + 2]]
+            else:
+                value2 = prog[position + 2]
+        if value1 == value2:
+            prog[prog[position + 3]] = 1
+        else:
+            prog[prog[position + 3]] = 0
+        return prog, position + 4
+    except Exception:
+        print('error in new op 7')
